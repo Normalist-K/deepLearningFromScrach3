a90bb15426c9ffb40babb98585a004b904e380c2
Normalist-K <normalkem@kakao.com>
2020-12-09 15:24:56 +0900

    ~step6

---
+++ steps/step02.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+data = np.array(3.0)
+x = Variable(data)
+f = Square()
+y = f(x)
+print(type(y), y.data)
