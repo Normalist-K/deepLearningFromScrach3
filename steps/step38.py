05d3fe5eda6f0d93fa3b27f31491f86740dd21c3
Normalist-K <normalkem@kakao.com>
2020-12-22 13:29:51 +0900

    형  형   형

---
+++ steps/step38.py
@@ -0,0 +1,19 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    import platform
+    if platform.system() == 'Darwin':
+        sys.path.append(
+            '/Users/youngin/anaconda3/envs/py-env/lib/python3.7/site-packages')
+
+import numpy as np
+from dezero import Variable
+from dezero.utils import plot_dot_graph
+import dezero.functions as F
+
+x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+y = F.reshape(x, (6,))
+y.backward(retain_grad = True)
+print(x.grad)
